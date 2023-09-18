3e0b9354bb5354f6737c0dc55b4391950e76e932
ashuchahalashuchahal173 <ashuchahalashuchahal173@gmail.com>
2023-09-19 00:58:52 +0530

    ashi

--- save_data.php
+++ save_data.php
@@ -13,27 +13,36 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve data from the form
+// Retrieve data from the form and sanitize
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Insert data into the database
+// Validate and sanitize inputs (example)
+$name = filter_var($name, FILTER_SANITIZE_STRING);
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+// Hash the password
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+// Insert data into the database using prepared statements
 $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $name, $email, $password);
+$stmt->bind_param("sss", $name, $email, $passwordHash);
 
 if ($stmt->execute()) {
     // Data inserted successfully, redirect to another page
     header("Location: index.html");
     exit(); // Make sure to exit the script after the redirection
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    // Log the error and display a user-friendly message
+    error_log("Error: " . $sql . "\n" . $stmt->error, 0); // Log the error
+    echo "Error: An error occurred while processing your request. Please try again later.";
 }
 
 // Close the database connection
 $stmt->close();
 $conn->close();
-?>
+
 
 
