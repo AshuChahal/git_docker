11b19196c7af0b79241c9bcca5a0c4603bc1fbc3
ashuchahalashuchahal173 <ashuchahalashuchahal173@gmail.com>
2023-09-19 08:46:32 +0530

    asd

--- save_data.php
+++ save_data.php
@@ -6,43 +6,29 @@ $password = 'dc@ashu12AS';
 $database = 'mydb';
 
 // Create a connection to the database
+
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check for connection errors
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve data from the form and sanitize
+// Get data from the form
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Validate and sanitize inputs (example)
-$name = filter_var($name, FILTER_SANITIZE_STRING);
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+// Insert data into the database
+$sql = "INSERT INTO users (name, email,password) VALUES ('$name', '$email','$password')";
 
-// Hash the password
-$passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-// Insert data into the database using prepared statements
-$sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $name, $email, $passwordHash);
-
-if ($stmt->execute()) {
-    // Data inserted successfully, redirect to another page
-    header("Location: index.html");
-    exit(); // Make sure to exit the script after the redirection
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully!";
 } else {
-    // Log the error and display a user-friendly message
-    error_log("Error: " . $sql . "\n" . $stmt->error, 0); // Log the error
-    echo "Error: An error occurred while processing your request. Please try again later.";
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 // Close the database connection
-$stmt->close();
+// $stmt->close();
 $conn->close();
 
-
+?>
 
